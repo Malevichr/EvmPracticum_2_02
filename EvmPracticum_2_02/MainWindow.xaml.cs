b2814ea5f74a3c1d3fc56a4160fd9c15cfc5fc18
Danila <rexerofficial@gmail.com>
2024-03-01 09:11:26 +0300

    Add project files.

---
+++ EvmPracticum_2_02/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvmPracticum_2_02
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Image opener program, made by Danila", "About program");
+        }
+
+        private void MenuItemOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.Jf; *.png;*.ico; *.emf; *.wmf";
+            if (fileDialog.ShowDialog() == true)
+            {
+                //fileDialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.Jf; *.png; *.ico; *.emf; *.wmf";
+                string fileName = fileDialog.FileName;
+                image.Source = new BitmapImage(new Uri(fileName));
+            }
+        }
+        private void MenuItemClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
